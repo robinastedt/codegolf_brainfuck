99f63baad8297404585294c2f3df3da636300494
Robin Åstedt <robin.astedt@gmail.com>
2020-06-05 19:43:37 +0200

    Used xor and comma operator to trim it a bit more

--- bf.c
+++ bf.c
@@ -12,8 +12,8 @@ b = bracket counter
 I = instruction
 S = skip function
 
-White space removed (252 bytes):
-i;p;q;b;I;char*c;S(){b=1;while(b){*++c==91?++b:*c==93?--b:0;}}main(int n,char**a){for (i=1;i<n;i++) {p=q=0;int A[99]={0};int*s[9];c=a[i];while(*c){I=*c;I==46?putchar(A[p]):I<91?*(I&16?&p:A+p)+=(I&2?1:-1):I==93?c=s[--q]:A[p]?s[q++]=c:S();I!=93?c++:0;}}}
+White space removed (247 bytes):
+i;p;q;b;I;char*c;S(){b=1;while(b){*++c==91?++b:*c==93?--b:0;}}main(n,a)char**a;{for (i=1,p=q=0;i<n;i++) {int A[99]={0};int*s[9];c=a[i];while(*c)I=*c,I==46?putchar(A[p]):I<91?*(I&16?&p:A+p)+=(I&2?1:-1):I==93?c=s[--q]:A[p]?s[q++]=c:S(),I^93?c++:0;}}
 */
 
 i;p;q;b;I;
@@ -24,20 +24,18 @@ S(){
         *++c==91?++b:*c==93?--b:0;
     }
 }
-main(int n,char**a){
-    for (i=1;i<n;i++) {
-        p=q=0;
+main(n,a)char**a;{
+    for (i=1,p=q=0;i<n;i++) {
         int A[99]={0};
         int*s[9];
         c=a[i];
-        while(*c){
-            I=*c;
+        while(*c)
+            I=*c,
             I==46?putchar(A[p]):
             I<91?*(I&16?&p:A+p)+=(I&2?1:-1):
             I==93?c=s[--q]:
             A[p]?s[q++]=c:
-            S();
-            I!=93?c++:0;
-        }
+            S(),
+            I^93?c++:0;
     }
 }
