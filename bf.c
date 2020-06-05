55ea146c2c54954e831e4834dcc28ff129ec5219
Robin Åstedt <robin.astedt@gmail.com>
2020-06-05 17:36:44 +0200

    Some more shavings... "if" is so ugly...

--- bf.c
+++ bf.c
@@ -13,7 +13,7 @@ I = instruction
 S = skip function
 
 White space removed (292 bytes):
-i;p;q;b;I;char*c;S(){b=0;do{*c=='['?++b:*c==']'?--b:0;++c;}while(b);c--;}main(int n,char** a){for (i=1;i<n;i++) {p=q=0;int A[99]={0};int*s[9];c=a[i];while(*c){I=*c;I=='>'?p++:I=='<'?p--:I=='+'?A[p]++:I=='-'?A[p]--:I=='.'?putchar(A[p]):I==']'?c = s[--q]:A[p]?s[q++]=c:S();if(I==']')c--;++c;}}}
+i;p;q;b;I;char*c;S(){b=0;do{*c=='['?++b:*c==']'?--b:0;++c;}while(b);c--;}main(int n,char** a){for (i=1;i<n;i++) {p=q=0;int A[99]={0};int*s[9];c=a[i];while(*c){I=*c;I=='>'?p++:I=='<'?p--:I=='+'?A[p]++:I=='-'?A[p]--:I=='.'?putchar(A[p]):I==']'?c = s[--q]:A[p]?s[q++]=c:S();I!=']'?c++:0;}}}
 */
 
 i;p;q;b;I;
@@ -52,8 +52,7 @@ main(int n,char** a){
             A[p]?
             s[q++]=c:
             S();
-            if(I==']')c--;
-            ++c;
+            I!=']'?c++:0;
         }
     }
 }
