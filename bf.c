b033c89addf818f454c9146752e5619dd0d7ed97
Robin Åstedt <robin.astedt@gmail.com>
2020-06-05 18:00:07 +0200

    Replaced char literals with int literals

--- bf.c
+++ bf.c
@@ -12,20 +12,18 @@ b = bracket counter
 I = instruction
 S = skip function
 
-White space removed (273 bytes):
-i;p;q;b;I;char*c;S(){b=1;while(b){*++c=='['?++b:*c==']'?--b:0;}}main(int n,char**a){for(i=1;i<n;i++){p=q=0;int A[99]={0};int*s[9];c=a[i];while(*c){I=*c;I=='>'?p++:I=='<'?p--:I=='+'?A[p]++:I=='-'?A[p]--:I=='.'?putchar(A[p]):I==']'?c=s[--q]:A[p]?s[q++]=c:S();I!=']'?c++:0;}}}
+White space removed (266 bytes):
+i;p;q;b;I;char*c;S(){b=1;while(b){*++c==91?++b:*c==93?--b:0;}}main(int n,char**a){for (i=1;i<n;i++) {p=q=0;int A[99]={0};int*s[9];c=a[i];while(*c){I=*c;I==62?p++:I==60?p--:I==43?A[p]++:I==45?A[p]--:I==46?putchar(A[p]):I==93?c=s[--q]:A[p]?s[q++]=c:S();I!=93?c++:0;}}}
 */
 
 i;p;q;b;I;
 char*c;
-
 S(){
     b=1;
     while(b){
-        *++c=='['?++b:*c==']'?--b:0;
+        *++c==91?++b:*c==93?--b:0;
     }
 }
-
 main(int n,char**a){
     for (i=1;i<n;i++) {
         p=q=0;
@@ -34,15 +32,15 @@ main(int n,char**a){
         c=a[i];
         while(*c){
             I=*c;
-            I=='>'?p++:
-            I=='<'?p--:
-            I=='+'?A[p]++:
-            I=='-'?A[p]--:
-            I=='.'?putchar(A[p]):
-            I==']'?c=s[--q]:
+            I==62?p++:
+            I==60?p--:
+            I==43?A[p]++:
+            I==45?A[p]--:
+            I==46?putchar(A[p]):
+            I==93?c=s[--q]:
             A[p]?s[q++]=c:
             S();
-            I!=']'?c++:0;
+            I!=93?c++:0;
         }
     }
 }
