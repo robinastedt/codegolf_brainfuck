2feff14755992dc62c5a148a1d2ea1b0d1973871
Robin Åstedt <robin.astedt@gmail.com>
2020-06-05 17:33:42 +0200

    Slight improvement by 2 bytes

--- bf.c
+++ bf.c
@@ -12,8 +12,8 @@ b = bracket counter
 I = instruction
 S = skip function
 
-White space removed (294 bytes):
-i;p;q;b;I;char*c;S(){b=0;do{*c=='['?++b:*c==']'?--b:0;++c;}while(b);c--;}main(int n,char** a){for (i=1;i<n;i++) {p=0;int A[99]={0};int*s[9];q=0;c=a[i];while(*c){I=*c;I=='>'?p++:I=='<'?p--:I=='+'?A[p]++:I=='-'?A[p]--:I=='.'?putchar(A[p]):I==']'?c = s[--q]:A[p]?s[q++]=c:S();if(I==']')c--;++c;}}}
+White space removed (292 bytes):
+i;p;q;b;I;char*c;S(){b=0;do{*c=='['?++b:*c==']'?--b:0;++c;}while(b);c--;}main(int n,char** a){for (i=1;i<n;i++) {p=q=0;int A[99]={0};int*s[9];c=a[i];while(*c){I=*c;I=='>'?p++:I=='<'?p--:I=='+'?A[p]++:I=='-'?A[p]--:I=='.'?putchar(A[p]):I==']'?c = s[--q]:A[p]?s[q++]=c:S();if(I==']')c--;++c;}}}
 */
 
 i;p;q;b;I;
@@ -31,10 +31,9 @@ S(){
 
 main(int n,char** a){
     for (i=1;i<n;i++) {
-        p=0;
+        p=q=0;
         int A[99]={0};
         int*s[9];
-        q=0;
         c=a[i];
         while(*c){
             I=*c;
