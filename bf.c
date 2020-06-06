3cfcf78a118a829240a9a4a230cf02a2f4b8673b
Robin Åstedt <robin.astedt@gmail.com>
2020-06-06 18:14:38 +0200

    Moved program counter increment to for loop header

--- bf.c
+++ bf.c
@@ -12,8 +12,8 @@ b = bracket counter
 I = instruction
 S = skip function
 
-White space removed (230 bytes):
-p;q;I;char*c;S(b){while(b)*++c==91?++b:*c==93?--b:0;}main(n,a)char**a;{int*s[9],A[99]={0};for(p=q=0,c=a[1];*c;)I=*c,I==46?putchar(A[p]):I<91?*(I&16?&p:A+p)+=(I&2)-1:I>91?c=s[--q]:A[p]?s[q++]=c:S(1),I^93?c++:0;n>2?main(n-1,a+1):0;}
+White space removed (227 bytes):
+p;q;I;char*c;S(b){while(b)*++c==91?++b:*c==93?--b:0;}main(n,a)char**a;{int*s[9],A[99]={0};for(p=q=0,c=a[1];*c;c+=I!=93)I=*c,I==46?putchar(A[p]):I<91?*(I&16?&p:A+p)+=(I&2)-1:I>91?c=s[--q]:A[p]?s[q++]=c:S(1);n>2?main(n-1,a+1):0;}
 */
 
 p;q;I;
@@ -24,13 +24,12 @@ S(b){
 }
 main(n,a)char**a;{
     int*s[9],A[99]={0};
-    for(p=q=0,c=a[1];*c;)
+    for(p=q=0,c=a[1];*c;c+=I!=93)
         I=*c,
         I==46?putchar(A[p]):
         I<91?*(I&16?&p:A+p)+=(I&2)-1:
         I>91?c=s[--q]:
         A[p]?s[q++]=c:
-        S(1),
-        I^93?c++:0;
+        S(1);
     n>2?main(n-1,a+1):0;
 }
