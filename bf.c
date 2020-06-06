efdf2a3468859e8a9081af1d4fac492c2b1db491
Robin Åstedt <robin.astedt@gmail.com>
2020-06-06 18:33:36 +0200

    Added some decimal and binary representations of characters in comments

--- bf.c
+++ bf.c
@@ -14,6 +14,15 @@ S = skip function
 
 White space removed (220 bytes):
 p;q;I;char*c;S(b){while(b)b+=*++c>90?92-*c:0;}main(n,a)char**a;{int*s[9],A[99]={0};for(p=q=0,c=a[1];*c;c+=I!=93)I=*c,I==46?putchar(A[p]):I<91?*(I&16?&p:A+p)+=(I&2)-1:I>91?c=s[--q]:A[p]?s[q++]=c:S(1);n>2?main(n-1,a+1):0;}
+
++ = 43, 0010 1011
+- = 45, 0010 1101
+. = 46, 0010 1110
+< = 60, 0011 1100
+> = 62, 0011 1110
+[ = 91, 0101 1011
+] = 93, 0101 1101
+
 */
 
 p;q;I;
