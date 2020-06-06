5843e2c48a286f20dee9e06487ef8ba808b35cd5
Robin Åstedt <robin.astedt@gmail.com>
2020-06-06 20:02:55 +0200

    Moved argv increment into for loop declaration

--- bf.c
+++ bf.c
@@ -12,7 +12,7 @@ i = instruction
 S = skip function
 
 White space removed (217 bytes):
-p;q;i;char*c;S(b){while(b)b+=*++c>90?92-*c:0;}main(n,a)int**a;{int*s[9],A[99]={0};for(p=q=0,c=a[1];*c;c+=i!=93)i=*c,i^46?i<91?*(i&16?&p:A+p)+=(i&2)-1:i>91?c=s[--q]:A[p]?s[q++]=c:S(1):putchar(A[p]);n>2&&main(n-1,a+1);}
+p;q;i;char*c;S(b){while(b)b+=*++c>90?92-*c:0;}main(n,a)int**a;{int*s[9],A[99]={0};for(p=q=0,c=*++a;*c;c+=i!=93)i=*c,i^46?i<91?*(i&16?&p:A+p)+=(i&2)-1:i>91?c=s[--q]:A[p]?s[q++]=c:S(1):putchar(A[p]);n>2&&main(n-1,a);}
 
 + = 43, 0010 1011
 - = 45, 0010 1101
@@ -32,7 +32,7 @@ S(b){
 }
 main(n,a)int**a;{
     int*s[9],A[99]={0};
-    for(p=q=0,c=a[1];*c;c+=i!=93)
+    for(p=q=0,c=*++a;*c;c+=i!=93)
         i=*c,
         i^46?
         i<91?*(i&16?&p:A+p)+=(i&2)-1:
@@ -40,5 +40,5 @@ main(n,a)int**a;{
         A[p]?s[q++]=c:
         S(1):
         putchar(A[p]);
-    n>2&&main(n-1,a+1);
+    n>2&&main(n-1,a);
 }
