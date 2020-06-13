a725ccb8b94c6c1417b1eb875f14aade8a734484
Robin Åstedt <robin.astedt@gmail.com>
2020-06-13 14:31:35 +0200

    Fixed character count

--- bf.c
+++ bf.c
@@ -11,7 +11,7 @@ b = bracket counter
 i = instruction
 S = skip function
 
-White space removed (217 bytes):
+White space removed (215 bytes):
 p;q;i;char*c;S(b){while(b)b+=*++c>90?92-*c:0;}main(n,a)int**a;{int*s[9],A[99]={0};for(p=q=0,c=*++a;*c;c+=i!=93)i=*c,i^46?i<91?*(i&16?&p:A+p)+=(i&2)-1:i>91?c=s[--q]:A[p]?s[q++]=c:S(1):putchar(A[p]);n>2&&main(n-1,a);}
 
 + = 43, 0010 1011
